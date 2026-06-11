68f7b6c465939326088ae73c6faef70f69ccd4ee
kavinn-sma <kavin.shanmalli@gmail.com>
2026-06-11 11:13:26 +0530

    Time: 3 ms (41.76%), Space: 46.7 MB (45.15%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,50 @@
+class Solution {
+    Boolean isSafe(int row, int col, int n, int [][] board){
+        //row
+        for(int c=0;c<=col-1;c++){
+            if(board[row][c]==1) return false;
+        }
+        //upper diagonal
+        for(int c=col,r=row;c>=0 && r>=0;c--,r--){
+            if(board[r][c]==1) return false;
+        } 
+        //lower diagonal
+        for(int c=col,r=row;c>=0 && r<=n-1;c--,r++){
+            if(board[r][c]==1) return false;
+        }      
+        return true;  
+    }
+    void addToAns(int [][] board, List<List<String>> ans){
+        List<String> temp= new ArrayList<>();
+        for(int i=0;i<board.length;i++){
+            StringBuilder s = new StringBuilder();
+            for(int j=0;j<board[0].length;j++){
+                if(board [i][j]==1) s.append('Q');
+                else s.append('.');
+            }
+            temp.add(s.toString());
+        }
+        ans.add(temp);
+    }
+    void f(int col, int n, int [][] board,List<List<String>> ans){
+        if(col==n){
+            //add board to the ans
+            addToAns(board,ans);
+            return ;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,n,board)){
+                board[row][col]=1;
+                f(col+1,n,board,ans);
+                //backtrack
+                board [row][col]=0;
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        int [][] board= new int [n][n];
+        f(0,n,board,ans);
+        return ans;
+    }
+}
